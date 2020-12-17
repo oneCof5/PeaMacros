5e931efcedf4c23b34f1b724320bf22442b73f85
Chris Pearson <oneCof5@gmail.com>
2020-12-17 10:26:47 -0500

    bloat cleanup; slash command support; reset to class/spec defaults

--- core.lua
+++ core.lua
@@ -1,66 +1,32 @@
-PeaMacros = { }
+local PeaMacros = CreateFrame("Frame", (...), UIParent)
+PeaMacros:RegisterEvent("PLAYER_LOGIN")
+
+-- Generic event handler
+PeaMacros:SetScript("OnEvent", function(self, event, ...)
+	self[event](self, ...)
+end)
+
 local class, currentSpec, SpecID, oldSpecID
 local QUESTION_MARK = "INV_Misc_QuestionMark"
 
-local PMF = CreateFrame("Frame", (...), UIParent)
-PMF:RegisterEvent("PLAYER_LOGIN")
+-- HELPER FUNCTIONS SECTION
 
-PMF:SetScript("OnEvent", function(self, event, ...)
---	print("Responding to ",event)
-	PMF[event](self, ...)
-end)
-
-function PMF:PLAYER_LOGIN()
-	-- Get the current character class
-	class = select(2,UnitClass("player"))
-
-	-- Get their current Specialization
-	currentSpec = GetSpecialization()
-
-	-- SpecializationID see the handy list via https://wow.gamepedia.com/SpecializationID
-	SpecID = GetSpecializationInfo(currentSpec)
-	oldSpecID = SpecID
---	print("SpecID=",SpecID," and oldSpecID=",oldSpecID)
-
-	-- Get saved variables
-	PeaMacros:SetDefaults()
-
-	-- start watching changes in macros and talent specs
-	PMF:RegisterUnitEvent("PLAYER_SPECIALIZATION_CHANGED", "player")
-	PMF:RegisterEvent("UPDATE_MACROS", "player")
-	PMF:UnregisterEvent("PLAYER_LOGIN")
-
+-- Print Function
+function PeaMacros:PeaPrint(msg)
+	print("|cffff00ffPeaMacros|r: " .. msg)
 end
 
-function PMF:PLAYER_SPECIALIZATION_CHANGED()
-	-- check if we're still the same spec
-	-- this fires when talents are changed in the spec
+-- update our spec info
+function PeaMacros:UpdatePlayerSpecInfo()
 
 	currentSpec = GetSpecialization()
 	SpecID = GetSpecializationInfo(currentSpec)
---	print("SpecID=",SpecID," and oldSpecID=",oldSpecID)
-	if SpecID ~= oldSpecID then
-		PeaMacros:SwapMacros() -- new spec, reload
-	end
-	-- save for later
-	oldSpecID = SpecID
 
-end
-
-function PMF:UPDATE_MACROS()
-	-- check each local macro and update saved variables for this spec.
-	-- this saves the user's personal preference over the default addon values
-	for i=121,138 do
-		local n, i, b = GetMacroInfo(i)
-		if PeaMacrosDB.ClassMacros[SpecID][n] then
-			PeaMacrosDB.ClassMacros[SpecID][n] = b
-		end
-	end
-	-- To Do:  Add in a checker to see if each of the macros we expect is still here?
 end
 
 function PeaMacros:SetDefaults()
 
+	-- PeaMacrosDB is our per-character saved variable
 	if not PeaMacrosDB then
 		PeaMacrosDB = {} -- establish empty table
 
@@ -107,64 +73,94 @@ function PeaMacros:SetDefaults()
 
 end
 
--- helper function to sort the macro table
-function PeaMacros:TableSort(t, order)
-  -- collect the keys
-  local keys = {}
-  for k in pairs(t) do keys[#keys+1] = k end
-
-  -- if order function given, sort by it by passing the table and keys a, b,
-  -- otherwise just sort the keys
-  if order then
-      table.sort(keys, function(a,b) return order(t, a, b) end)
-  else
-      table.sort(keys)
-  end
-
-  -- return the iterator function
-  local i = 0
-  return function()
-      i = i + 1
-      if keys[i] then
-          return keys[i], t[keys[i]]
-      end
-  end
-end
-
+-- Update the macros
 function PeaMacros:SwapMacros()
 
-	-- Update the macros
-	for name,body in PeaMacros:TableSort(PeaMacrosDB.ClassMacros[SpecID]) do
-		-- check if this macro exists
-		local mexists = GetMacroIndexByName(name)
-		if mexists ~= 0 then -- macro found with this name, edit the existing
-			EditMacro(mexists, name, nil, body)
+	local ta = PeaMacrosDB.ClassMacros[SpecID] -- capture the new spec section
+	local to = PeaMacrosDB.ClassMacros[oldSpecID] -- capture the prev spec section
+
+	-- iterate over our DB
+	for name,body in next, ta do
+
+		-- check if the player already has a macro by this name
+		local myMacroIndex = GetMacroIndexByName(name)
+
+		if myMacroIndex ~= 0 then -- macro found with this name
+
+			-- Save the macro text if different than our DB
+			local myMacroBody = select(3,GetMacroInfo(myMacroIndex))
+
+			if to[name] ~= myMacroBody then
+				to[name] = myMacroBody
+			end
+
+			EditMacro(myMacroIndex, name, nil, body)
+
 		else -- macro not found, so create it if there's room
 			local _, numMacs = GetNumMacros()
 			if numMacs >= 17 then
-				print("You've got too many character macros, chief.")
-				return
+				PeaMacros:PeaPrint("You've got too many character macros, chief.")
+				do return end
 			else
 				CreateMacro(name, QUESTION_MARK, body, 1)
-				print("Created ", name, " for you!")
+				PeaMacros:PeaPrint("Created ", name, " for you!")
 			end
+		end
+
+		-- Update our DB
+		for k,v in next, PeaMacrosDB.ClassMacros[oldSpecID] do
+			PeaMacrosDB.ClassMacros[oldSpecID][k] = to[k]
 		end
 
 	end
 
-	-- Update the Macro Panel if needed
+	-- Update the Blizzard Macro Frame if needed
 	if MacroFrame and MacroFrame:IsShown() then
 		MacroFrame_Update()
 	end
 
 end
 
+-- ON EVENT SECTION
+function PeaMacros:PLAYER_SPECIALIZATION_CHANGED()
+	-- this fires on a talent change or a specialization change
+
+	PeaMacros:UpdatePlayerSpecInfo()
+
+	if SpecID ~= oldSpecID then
+		PeaMacros:SwapMacros() -- new spec, reload
+		oldSpecID = SpecID -- save for later
+	end
+
+end
+
+-- Initialization on PLAYER_LOGIN
+function PeaMacros:PLAYER_LOGIN()
+
+	-- Get the current character class and specialization
+	class = select(2,UnitClass("player"))
+	PeaMacros:UpdatePlayerSpecInfo()
+	oldSpecID = SpecID -- save for later
+
+	-- Get saved variables
+	PeaMacros:SetDefaults()
+
+	-- start watching for changes to specialization
+	PeaMacros:RegisterUnitEvent("PLAYER_SPECIALIZATION_CHANGED", "player")
+
+	-- clean up unnecessary watch
+	PeaMacros:UnregisterEvent("PLAYER_LOGIN")
+end
+
+--[[
+SLASH COMMAND SECTION
+]]
 SlashCmdList["PEAMACROS"] = function(arg)
 	if arg == "reset" then
 		PeaMacros:SetDefaults()
 		PeaMacros:SwapMacros()
 	else
-		print("To reset macros, you must enter '/pmac reset'. ")
+		PeaMacros:PeaPrint("To reset macros, you must enter '/pmac reset'. ")
 	end
 end
 SLASH_PEAMACROS1 = "/pmac"
